9661ac907bf47498e82dae3e2ace1fdaaea92cc8
Planeshifter <pburckhardt@outlook.com>
2014-08-29 11:45:03 -0400

    pruning included

--- server/prunedTreeCreation.js
+++ server/prunedTreeCreation.js
@@ -1,5 +1,4 @@
 var tm = require("text-miner");
-var traceur = require("traceur");
 var wn = require("wordnet-magic");
 var Promise = require("bluebird");
 var _ = require("underscore");
@@ -8,44 +7,19 @@ var ndarray = require("ndarray");
 var ops = require("ndarray-ops");
 var unpack = require("ndarray-unpack")
 var arraySugar = require("./array.js")
+
 var idStore = [];
 
-exports.analyzeCorpus = function(arr){
-  console.log(arr)
-  var treesPromise = arr.map(doc => getHypernymTree(doc))
-  var mergedTreePromise = Promise.all(treesPromise).then(elem => mergeDocumentTrees(elem));
-  return mergedTreePromise;
-}
+module.exports = getHypernymTree = function(arr){
 
-function getHypernymTree(sentence){
-	var corpus = new tm.Corpus(Array(sentence));
-	corpus = corpus
-			.removeInterpunctuation()
-			.removeNewlines()
-			.toLower()
-			.removeWords(tm.STOPWORDS.EN)
-			.clean();
-
-	var wordArray = corpus.documents[0].split(" ");
-	wordArray = _.uniq(wordArray); // only keep single instances of duplicate words
-	var wordArrayPromise = wordArray.map(x => wn.morphy(x));
-	var wordArrayDone = Promise.all(wordArrayPromise);
-	var wordArrayPromise2 = wordArrayDone.then(elem => elem.filter(elem => elem.length >= 1)).then(elem => elem.map(arr => new wn.Word(arr[0].lemma,"n")));
-    var synsetArrayPromise = wordArrayPromise2.then(arr => arr.map(w => w.getSynsets()));		
-    var synsetArrayPromise2 = Promise.all(synsetArrayPromise);
-
-    var wordSynsetsPromiseAll = synsetArrayPromise2.then(function(wordSynsetsArray){
-    	return Promise.all(wordSynsetsArray.map(function(wordSynsets){
-     	  var wordSynsetsPromise = wordSynsets.map(function(s){
+     	var wordSynsetsPromise = arr.map(function(s){
 	        s.hypernym = s.getHypernymTree();
 	        return Promise.props(s);	
-	      });
-	    return Promise.all(wordSynsetsPromise);
-    	}));
-    });
+	    });
+	    var wordSynsetsPromiseAll = Promise.all(wordSynsetsPromise);
 
-    var dataPromise = wordSynsetsPromiseAll.then(function(arr){
-      var dataWordPromises = arr.map(function(wordSynsets){
+    var dataPromise = wordSynsetsPromiseAll.then(function(wordSynsets){
+   
       	var adjMatrix = ndarray(Int8Array(100 * 100), [100, 100]);
         var synsets = [];	
 
@@ -61,8 +35,7 @@ function getHypernymTree(sentence){
 	          var parentIndex = idStore.indexOf(obj.hypernym[0].synsetid);
 	          var childIndex = idStore.indexOf(obj.synsetid);
 			  adjMatrix.set(parentIndex, childIndex, 1);  	
-			}  
-
+			  
 	    	var selectedSynset = synsets.filter(elem => elem.synsetid == obj.synsetid);
 		    if (selectedSynset.length === 0){
 			  synsets.push(obj); 
@@ -155,49 +128,9 @@ function pruneDocumentTree(arr, wordSynsetsArray){
   });
 
   toKeepSynsets = _.flatten(toKeepSynsets);
-
-  return createPrunedTree(toKeepSynsets);
-}
-
-function createPrunedTree(toKeepSynsets){
-
-      var adjMatrix = ndarray(Int8Array(100 * 100), [100, 100]);
-      var synsets = []; 
-
-        function processObject(obj){
-          if (obj.hypernym.length !== 0){
-            if (!idStore.some(id => id === obj.hypernym[0].synsetid)){
-              idStore.push(obj.hypernym[0].synsetid);
-            }
-            if (!idStore.some(id => id === obj.synsetid)){
-              idStore.push(obj.synsetid);
-            }
-
-            var parentIndex = idStore.indexOf(obj.hypernym[0].synsetid);
-            var childIndex = idStore.indexOf(obj.synsetid);
-        adjMatrix.set(parentIndex, childIndex, 1);    
-      }  
-
-        var selectedSynset = synsets.filter(elem => elem.synsetid == obj.synsetid);
-        if (selectedSynset.length === 0){
-          synsets.push(obj); 
-        } 
-        if (obj.hypernym.length !== 0) processObject(obj.hypernym[0]);
-        return;  
-      }
-      toKeepSynsets.forEach(elem => processObject(elem));
-
-      synsets = synsets.map(elem => {
-        // delete elem.hypernym;
-        elem.counter = 1;
-        return elem;
-      }); 
-
-      var data = {};
-      data.synsets = synsets;
-      data.adjMatrix = adjMatrix;
-      console.log(data)
-      return data;
+ 
+  console.log(arr.synsets.length)
+  return arr;
 }
 
 function mergeDocumentTrees(arr){
